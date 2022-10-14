a925ca613cae81ea1b95065cfc57c3aed4bc5bfe
skoved <skoved@redhat.com>
2022-10-14 14:29:14 -0400

    use new config types

--- config.go
+++ config.go
@@ -3,15 +3,11 @@ package otelutils
 import (
 	"context"
 	"fmt"
-	"os"
 	"strings"
 
-	"github.com/spf13/viper"
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/propagation"
 	sdkTrace "go.opentelemetry.io/otel/sdk/trace"
-	otelTrace "go.opentelemetry.io/otel/trace"
 )
 
 // supported otel exporters
@@ -29,31 +25,14 @@ var (
 	serviceName string
 )
 
-// initializes the otel configuration for the cli
-func OtelInit(name, otelExporter string) error {
-	serviceName = name
-	var exp sdkTrace.SpanExporter
-	resource, err := newResource()
-
+// initializes the otel configuration for the cli.
+func OtelInit(startCtx context.Context, resourceConf ResourceConfig, exporterConfig SpanExporterConfig) error {
+	resource, err := resourceConf.newResource(startCtx)
 	if err != nil {
 		return err
 	}
 
-	switch otelExporter {
-	case consoleExporter:
-		exp, err = newConsoleExporter(os.Stdout)
-	case fileExporter:
-		f, fErr := os.Create(viper.GetString("OTEL_FILE_SPAN_EXPORTER_NAME"))
-		if fErr != nil {
-			return fErr
-		}
-		exp, err = newConsoleExporter(f)
-	case otlpExporter:
-		exp, err = newOtlpExporter()
-	default:
-		return fmt.Errorf("Exporter type '%s' is not implemented", otelExporter)
-	}
-
+	exp, err := exporterConfig.newSpanExporter(startCtx)
 	if err != nil {
 		return err
 	}
@@ -69,11 +48,11 @@ func OtelInit(name, otelExporter string) error {
 }
 
 // flush and shutdown the global TracerProvider
-func OtelEnd() error {
-	if err := tp.ForceFlush(context.Background()); err != nil {
+func OtelEnd(endCtx context.Context) error {
+	if err := tp.ForceFlush(endCtx); err != nil {
 		return err
 	}
-	return tp.Shutdown(context.Background())
+	return tp.Shutdown(endCtx)
 }
 
 // GetTraceParentEnv returns the traceparent from ctx as an envvar
@@ -81,16 +60,4 @@ func GetTraceParentEnv(ctx context.Context) string {
 	carrier := propagation.MapCarrier{}
 	otel.GetTextMapPropagator().Inject(ctx, carrier)
 	return fmt.Sprintf("%s=%s", traceParent, carrier.Get(strings.ToLower(traceParent)))
-}
-
-// Starts a new span and returns the context of the new span and the span.
-// The returned span will be a child span of the span assiciated with parentCtx
-func StartSpan(parentCtx context.Context, spanName string, opts ...otelTrace.SpanStartOption) (context.Context, otelTrace.Span) {
-	return otel.Tracer(serviceName).Start(parentCtx, spanName, opts...)
-}
-
-// adds the error to the passed span. Should be called when a function throws an error
-func Error(span *otelTrace.Span, err error) {
-	(*span).RecordError(err)
-	(*span).SetStatus(codes.Error, err.Error())
 }
