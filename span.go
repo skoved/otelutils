461ec5e17afb4511f72e7ab6301c8bf89ac88b85
skoved <skoved@redhat.com>
2022-10-14 14:27:37 -0400

    separate file for span functions

---
+++ span.go
@@ -0,0 +1,30 @@
+package otelutils
+
+import (
+	"context"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/codes"
+	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
+	otelTrace "go.opentelemetry.io/otel/trace"
+)
+
+const libName = "github.com/skoved/otelutils"
+const libVer = "pre-release"
+
+var tracerOpts = []otelTrace.TracerOption{
+	otelTrace.WithInstrumentationVersion(libVer),
+	otelTrace.WithSchemaURL(semconv.SchemaURL),
+}
+
+// Starts a new span and returns the context of the new span and the span.
+// The returned span will be a child span of the span assiciated with parentCtx
+func StartSpan(parentCtx context.Context, spanName string, spanOpts ...otelTrace.SpanStartOption) (context.Context, otelTrace.Span) {
+	return otel.Tracer(libName, tracerOpts...).Start(parentCtx, spanName, spanOpts...)
+}
+
+// adds the error to the passed span. Should be called when a function throws an error
+func Error(span *otelTrace.Span, err error) {
+	(*span).RecordError(err)
+	(*span).SetStatus(codes.Error, err.Error())
+}
